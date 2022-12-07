ef0ba6b8fe96c7462390b725797fcb65d88f6da1
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-12-07 11:10:49 +0530

    secondt push

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const configuration = {
   SiteName: 'NFJ LABS',
   Use2DTextOver3D: false, // Change to true if you want 2D over 3D
   SiteNameSize: 0.7, // Between 0 and +
-  NumberOfVerticalLines: 25,
+  NumberOfVerticalLines: 7,
   NumberOfDots: 5000,
   // NumberOfJews: 1000,
   // NumberOfRings: 1000,
